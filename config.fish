de0244448a2752ae4bd699a9eaf822402d042c35
Jan Kiene <jankiene@onlinehome.de>
2017-09-22 16:43:22 +0200

    Move path adjustments to /etc/profile.d (where they belong), same for env vars

--- config.fish
+++ config.fish
@@ -1,14 +1,2 @@
 ## aliases
 alias update "sudo apt update; sudo apt upgrade -y"
-
-## environment variables
-set -gx JAVA_HOME "/usr/lib/jvm/java-8-oracle/jre"
-
-set -gx BOOST_ROOT "/opt/boost/boost_1_61_0"
-
-## PATH adjustments
-set -l PATH_OPT_BIN "/opt/bin"
-
-set -l PATH_USER_BIN "/home/jan/bin"
-
-set -gx PATH $PATH $PATH_OPT_BIN $PATH_USER_BIN
